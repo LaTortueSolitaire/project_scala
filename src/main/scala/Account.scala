63e91718e27059c9131cd65b34accb8b4e476ff5
foo-bar-sketch <danilin@stud.ntnu.no>
2018-10-15 10:37:12 +0200

    changed syncing in Account..

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -12,15 +12,15 @@ class Account(val bank: Bank, initialBalance: Double) {
       case amount if getBalanceAmount < amount => throw new NoSufficientFundsException("Not enough funds available.")
       case _ => this.balance.amount = this.balance.amount - amount
     }
+
     def deposit(amount: Double): Unit = amount match {
       case amount if amount <= 0 => throw new IllegalAmountException("Amount must be larger than zero.")
       case _ => this.balance.amount = this.balance.amount + amount
     }
-    def getBalanceAmount: Double = this.balance.amount
+
+    def getBalanceAmount: Double = synchronized { this.balance.amount }
 
     def transferTo(account: Account, amount: Double) = {
         bank addTransactionToQueue (this, account, amount)
     }
-
-
 }
